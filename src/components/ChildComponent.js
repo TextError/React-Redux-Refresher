56812476ddf32395c8d1342fade7c097348764c4
TextError <savinraul1983@yahoo.com>
2019-04-15 18:44:27 +0100

    parent & child component + boostrap

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export default class ChildComponent extends Component {
+
+class ChildComponent extends Component {
   render() {
     return (
-      <div>
-        
+      <div className='child'>
+        <div className='row'>
+          <div className='col text-center mt-5 d-flex'>
+            <div className='child-title m-auto'>
+              <h5>Child -- Component</h5>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
-}
+};
+
+export default ChildComponent;
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export default class ParentComponent extends Component {
+class ParentComponent extends Component {
   render() {
     return (
-      <div>
-        
+      <div className='parent'>
+        <div className='row'>
+          <div className='col text-center mt-5 d-flex'>
+            <div className='parent-title m-auto'>
+              <h5>Parent -- Component</h5>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
-}
+};
+
+export default  ParentComponent;
